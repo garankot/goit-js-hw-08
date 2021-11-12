785709b9f5b5175bdfb94ce0ccf329330b6e3247
Taras Garan'ko <garanko.t@gmail.com>
2021-11-12 14:06:37 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,22 @@
 
-import
+import throttle from "lodash.throttle";
+import Player from "@vimeo/player";
+
+const player = new Player(document.querySelector("#vimeo-player"));
+const CURRENT_TIME = "videoplayer-current-time";
+
+player.on("timeupdate", throttle(timeupdate, 1000));
+
+if (localStorage.getItem(CURRENT_TIME)) {
+  player.setCurrentTime(Number.parseFloat(localStorage.getItem(CURRENT_TIME)));
+}
+
+function timeupdate(event) {
+  localStorage.setItem(CURRENT_TIME, event.seconds === event.duration ? 0 : event.seconds);
+}
 
 const onPlay = function(data) {
     // data is an object containing properties specific to that event
 };
 
-player.on('play', onPlay);
+player.on("play", onPlay);
