0b680e8401f5f30e0465ffffcb6e8686ffc53abb
Taras Garan'ko <garanko.t@gmail.com>
2021-11-15 13:20:39 +0200

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
 
+var throttle = require('lodash.throttle');
+const feedbackForm = document.querySelector(".feedback-form");
+
+const savedFormDataJSON = localStorage.getItem("feedback-form-state");
+const savedFormData = JSON.parse(savedFormDataJSON);
+
+if (savedFormData !== null) {
+    feedbackForm["email"].value = savedFormData.email;
+    feedbackForm["message"].value = savedFormData.message;
+};
+
